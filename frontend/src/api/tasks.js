28cfe52e8defc30ded1184b731e28d3e6cc0bfb0
s2ddv <1speedftn@gmail.com>
2026-01-23 18:22:05 -0300

    setup: start of frontend setup

---
+++ frontend/src/api/tasks.js
@@ -0,0 +1,7 @@
+api.interceptors.response.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
